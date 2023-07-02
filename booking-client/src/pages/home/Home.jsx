e14983651c566ab6f5306ae09ef9d22d603067cd
Blackflame6 <engineerfranklyn@gmail.com>
2023-07-02 11:52:22 -0500

    making list search component

--- booking-client/src/pages/home/Home.jsx
+++ booking-client/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
@@ -14,10 +15,11 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
-        <PropertyList  />
+        <PropertyList />
         <h1 className="homeTitle">Home guests love</h1>
         <FeaturedProperties />
-      <MailList />
+        <MailList />
+        <Footer />
       </div>
     </div>
   );
--- booking-client/src/pages/list/List.jsx
+++ booking-client/src/pages/list/List.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
+import "./list.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
