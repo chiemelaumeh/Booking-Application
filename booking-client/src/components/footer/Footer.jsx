3ff08706ae8ab5b2f625f98f59c16e660c0d76e5
Blackflame6 <engineerfranklyn@gmail.com>
2023-07-02 11:23:21 -0500

    making footerr items

--- booking-client/src/components/footer/Footer.jsx
+++ booking-client/src/components/footer/Footer.jsx
@@ -2,7 +2,19 @@ import "./footer.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className="fLists">
+        <div className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+ 
+        </div>
+      </div>
+    </div>
   )
 }
 
