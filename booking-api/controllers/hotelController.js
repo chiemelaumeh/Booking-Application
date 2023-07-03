b59789446dab1af27ca311948636fca13c02efbf
Blackflame6 <engineerfranklyn@gmail.com>
2023-07-03 16:05:19 -0500

    loaded controllers

---
+++ booking-api/controllers/hotelController.js
@@ -0,0 +1,56 @@
+import Hotel from "../models/HotelModel.js";
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(201).json(updatedHotel);
+  } catch (err) {
+    next(err)
+  }
+};
+
+export const deleteHotel = async(req, res, next) => {
+ try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(201).json("Hotel has been deleted");
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getOneHotel = async(req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(
+      req.params.id,
+    );
+   
+    res.status(201).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const getAllHotels = async(req, res, next) => {
+   try {
+    const hotels = await Hotel.find(
+    );
+   
+    res.status(201).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+}
