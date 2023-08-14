99db10642a41a1de68db781d6908a2ebb976c317
chiemelaumeh <chiemelaumeh1@gmail.com>
2023-08-13 23:38:22 -0500

    added add property text field

--- booking-client/src/pages/addProperty/AddProperty.jsx
+++ booking-client/src/pages/addProperty/AddProperty.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./addProperty.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import { useLocation } from "react-router-dom";
 import upload from "../../assets/upload.png";
 
@@ -8,44 +9,82 @@ const AddProperty = () => {
   return (
     <>
       <div className="addProperty">
+        <div className="addPropertyText">
+          <div className="title-address">
+            <div className="spanDiv">
+              <span className="label">Property Name</span>
+              <input type="text" className="title-address-input" />
+            </div>
+            <div className="spanDiv">
+              <span className="label">Property Address</span>
+              <input type="text" className="title-address-input" />
+            </div>
+            <div className="spanDiv">
+              <span className="label">Property Title</span>
+              <input type="text" className="title-address-input" />
+            </div>
+            <div className="spanDiv">
+              <span className="label">Property Desciption</span>
+              <textarea type="" className="title-address-input textarea" />
+            </div>
+          </div>
+          <div className="city-type">
+            <div className="spanDiv">
+              <span className="label">City</span>
+              <input type="text" className="city-type-input" />
+            </div>
+            <div className="spanDiv">
+              <span className="label">Property Type</span>
+              <input type="text" className="city-type-input" />
+            </div>
+            <div className="spanDiv">
+              <span className="label">Distance from city center</span>
+              <input type="text" className="city-type-input" />
+            </div>
+            <div className="spanDiv">
+              <span className="label">Cheapest Price</span>
+              <input type="text" className="city-type-input" />
+            </div>
+          </div>
+        </div>
         <div className="addPropertyContainer">
-          <div className="addPropertySection">
-            
-          </div>
-          <div className="addPropertySection">
-            <div className="imageRow">
-              <div className="imageSection">
-                <img src={upload} className="imageBox" alt="" />
-                <input className="rf" type="file" title=" " name="" id="" />
-              </div>
-              <div className="imageSection">
-                <img src={upload} className="imageBox" alt="" />
-                <input className="rf" type="file" name="" id="" />
-              </div>
-              <div className="imageSection">
-                <img src={upload} className="imageBox" alt="" />
-                <input className="rf" type="file" name="" id="" />
-              </div>
+          <div className="imageRow">
+            <div className="imageSection">
+              <img src={upload} className="imageBox" alt="" />
+              <input
+                className="choose-file"
+                type="file"
+                title=" "
+                name=""
+                id=""
+              />
             </div>
-            <div className="imageRow">
-              <div className="imageSection">
-                <img src={upload} className="imageBox" alt="" />
-                <input className="rf" type="file" name="" id="" />
-              </div>
-              <div className="imageSection">
-                <img src={upload} className="imageBox" alt="" />
-                <input className="rf" type="file" name="" id="" />
-              </div>
-              <div className="imageSection">
-                <img src={upload} className="imageBox" alt="" />
-                <input className="rf" type="file" name="" id="" />
-              </div>
+            <div className="imageSection">
+              <img src={upload} className="imageBox" alt="" />
+              <input className="choose-file" type="file" name="" id="" />
             </div>
-            <div className="submitContiner">
-
-            <button className="submitButton">Create Propery</button>
+            <div className="imageSection">
+              <img src={upload} className="imageBox" alt="" />
+              <input className="choose-file" type="file" name="" id="" />
             </div>
           </div>
+          <div className="imageRow">
+            <div className="imageSection">
+              <img src={upload} className="imageBox" alt="" />
+              <input className="choose-file" type="file" name="" id="" />
+            </div>
+            <div className="imageSection">
+              <img src={upload} className="imageBox" alt="" />
+              <input className="choose-file" type="file" name="" id="" />
+            </div>
+            <div className="imageSection">
+              <img src={upload} className="imageBox" alt="" />
+              <input className="choose-file" type="file" name="" id="" />
+            </div>
+          </div>
+          <div className="submitContiner">
+          </div>
+            <button className="submitButton">Create Property</button>
         </div>
       </div>
     </>
