df09f599f03dead4b90eded5a3609a90a89bf8be
chiemelaumeh <chiemelaumeh1@gmail.com>
2023-08-14 00:00:08 -0500

    created controlled components

--- booking-client/src/pages/addProperty/AddProperty.jsx
+++ booking-client/src/pages/addProperty/AddProperty.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import "./addProperty.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { useLocation } from "react-router-dom";
 import upload from "../../assets/upload.png";
 
 const AddProperty = () => {
+  const [propertyFields, setPropertyFields] = useState({
+    name: "",
+    address: "",
+    title: "",
+    desc: "",
+    city: "",
+    type: "",
+    distance: "",
+    cheapestPrice: 0,
+  });
+
+  const handleChange = (e) => {
+    setPropertyFields((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
   const location = useLocation();
   return (
     <>
@@ -13,37 +28,86 @@ const AddProperty = () => {
           <div className="title-address">
             <div className="spanDiv">
               <span className="label">Property Name</span>
-              <input type="text" className="title-address-input" />
+              <input
+                type="text"
+                value={propertyFields.name}
+                id="name"
+                className="title-address-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="spanDiv">
               <span className="label">Property Address</span>
-              <input type="text" className="title-address-input" />
+              <input
+                type="text"
+                value={propertyFields.address}
+                id="address"
+                className="title-address-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="spanDiv">
               <span className="label">Property Title</span>
-              <input type="text" className="title-address-input" />
+              <input
+                type="text"
+                value={propertyFields.title}
+                id="title"
+                className="title-address-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="spanDiv">
-              <span className="label">Property Desciption</span>
-              <textarea type="" className="title-address-input textarea" />
+              <span className="label">Property Description</span>
+              <textarea
+                type=""
+                value={propertyFields.desc}
+                id="desc"
+                className="title-address-input textarea"
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="city-type">
             <div className="spanDiv">
               <span className="label">City</span>
-              <input type="text" className="city-type-input" />
+              <input
+                type="text"
+                value={propertyFields.city}
+                id="city"
+                className="city-type-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="spanDiv">
               <span className="label">Property Type</span>
-              <input type="text" className="city-type-input" />
+              <input
+                type="text"
+                value={propertyFields.type}
+                id="type"
+                className="city-type-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="spanDiv">
               <span className="label">Distance from city center</span>
-              <input type="text" className="city-type-input" />
+              <input
+                type="text"
+                value={propertyFields.distance}
+                id="distance"
+                className="city-type-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="spanDiv">
               <span className="label">Cheapest Price</span>
-              <input type="text" className="city-type-input" />
+              <input
+                placeholder="Numbers Only"
+                value={propertyFields.cheapestPrice}
+                id="cheapestPrice"
+                type="number"
+                className="city-type-input"
+                onChange={handleChange}
+              />
             </div>
           </div>
         </div>
@@ -82,9 +146,8 @@ const AddProperty = () => {
               <input className="choose-file" type="file" name="" id="" />
             </div>
           </div>
-          <div className="submitContiner">
-          </div>
-            <button className="submitButton">Create Property</button>
+          <div className="submitContiner"></div>
+          <button className="submitButton">Create Property</button>
         </div>
       </div>
     </>
